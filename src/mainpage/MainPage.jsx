6fd86eceb42cc941809b888b640d2ec1e02cae8f
Diego <dfmontesinos@gmail.com>
2025-04-14 16:18:15 +0200

    code: add new routes

--- src/mainpage/MainPage.jsx
+++ src/mainpage/MainPage.jsx
@@ -4,12 +4,14 @@ import React, { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/api";
 import { listRoutes } from "../graphql/queries"; // Asegúrate de que la ruta sea correcta
 import "../App.css"; // Si tienes estilos, inclúyelos aquí
+import { createRoute } from "../graphql/mutations";
 
 const client = generateClient();
 
 const MainPage = () => {
   const [routes, setRoutes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [newRoute, setNewRoute] = useState({ title: "", description: "" });
 
   useEffect(() => {
     const fetchRoutes = async () => {
@@ -29,6 +31,26 @@ const MainPage = () => {
     fetchRoutes();
   }, []);
 
+  const handleCreateRoute = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await client.graphql({
+        query: createRoute,
+        variables: {
+          input: {
+            title: newRoute.title,
+            description: newRoute.description,
+          },
+        },
+      });
+
+      // Añadir nueva ruta a la lista sin necesidad de refetch
+      setRoutes([...routes, response.data.createRoute]);
+      setNewRoute({ title: "", description: "" });
+    } catch (err) {
+      console.error("Error creating route:", err);
+    }
+  };
   return (
     <div className="main-container">
       <h1>Lista de Rutas</h1>
@@ -44,6 +66,24 @@ const MainPage = () => {
           ))}
         </ul>
       )}
+      <form onSubmit={handleCreateRoute} className="form-crear-ruta">
+        <input
+          type="text"
+          placeholder="Título"
+          value={newRoute.title}
+          onChange={(e) => setNewRoute({ ...newRoute, title: e.target.value })}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Descripción"
+          value={newRoute.description}
+          onChange={(e) =>
+            setNewRoute({ ...newRoute, description: e.target.value })
+          }
+        />
+        <button type="submit">Crear Ruta</button>
+      </form>
     </div>
   );
 };
